9ef25f57ba51492bb4d852e0f6bbaa8a4d688643
CodeSeba <seba84x@gmail.com>
2018-03-24 00:55:26 -0300

    Corregido las Test de GenericR

--- TardeClase09MavenJDBCGenericDAO/src/main/java/ar/org/centro8/curso/java/repositories/GenericR.java
+++ TardeClase09MavenJDBCGenericDAO/src/main/java/ar/org/centro8/curso/java/repositories/GenericR.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenericR<E> {
+	
     private Connection conn;
     
     public GenericR(Connection conn) {
@@ -18,17 +19,22 @@ public class GenericR<E> {
     public void save(E e) {
         try {
             PreparedStatement ps = conn.prepareStatement(createQuery(e),1);
-            Field[] fields = e.getClass().getDeclaredFields();
-            for (int a=0 ; a<fields.length ; a++) {
+            
+			Field[] fields = e.getClass().getDeclaredFields();
+            
+			for (int a=0 ; a<fields.length ; a++) {
                 String name = fields[a].getName();
                 String method = "get" + name.substring(0,1).toUpperCase() + name.substring(1, name.length());
                 //System.out.println(method);
-                if (a==0) ps.setObject(1, null);
+                
+				if (a==0) ps.setObject(1, null);
                 else ps.setObject(a+1, e.getClass().getMethod(method, null).invoke(e));
             }
             ps.execute();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) e.getClass().getMethod("setId", int.class).invoke(e,rs.getInt(1));
+            
+			ResultSet rs = ps.getGeneratedKeys();
+            
+			if (rs.next()) e.getClass().getMethod("setId", int.class).invoke(e,rs.getInt(1));
         } catch (Exception ex) {
             System.out.println(ex);
         }
@@ -73,24 +79,29 @@ public class GenericR<E> {
         String query = "select * from ";
         List<E> lista = new ArrayList();
         try {
-            Class clazz = Class.forName("ar.org.centro8.curso.java.entities."+type);
+            Class clazz = Class.forName("ar.org.centro8.curso.java.entities." + type);
             e = (E)clazz.newInstance();
             String tabla = (String)clazz.getMethod("getTable", null).invoke(e);
             query += tabla + " where " + filtro;
-            //System.out.println(query);
-            ResultSet rs = conn.createStatement().executeQuery(query);
+            System.out.println(query);
+            
+			ResultSet rs = conn.createStatement().executeQuery(query);
             ResultSetMetaData rsmd = rs.getMetaData();
-            while (rs.next()) {
+            
+			while (rs.next()) {
                 for (int a=1 ; a<=rsmd.getColumnCount() ; a++) {
                     String field = rsmd.getColumnName(a);
                     String method = "set" + field.substring(0, 1).toUpperCase() + field.substring(1);
                     //System.out.println(method);
-                    if (rsmd.getColumnTypeName(a).equalsIgnoreCase("INT"))
+                    
+					if (rsmd.getColumnTypeName(a).equalsIgnoreCase("INT"))
                         e.getClass().getMethod(method, int.class).invoke(e, rs.getInt(field));
                     if (rsmd.getColumnTypeName(a).equalsIgnoreCase("VARCHAR"))
                         e.getClass().getMethod(method, String.class).invoke(e, rs.getString(field));
                 }
-                lista.add(e);
+                
+				lista.add(e);
+				e = (E)clazz.newInstance();
             }
         } catch (Exception ex) {
             System.out.println(ex);
@@ -99,12 +110,13 @@ public class GenericR<E> {
         return lista;
     }
     
-    public E getById(int id, String type) {
-        List<E> lista = getByFiltro("id=" + id, type);
-        return (lista == null || lista.isEmpty())? null:lista.get(0);
-    }
-    
     public List<E> getAll(String type) {
         return getByFiltro("1=1",type);
     }
+	
+	public E getById(int id, String type) {
+        List<E> lista = getByFiltro("id=" + id, type);
+        return (lista == null || lista.isEmpty())? null:lista.get(0);
+    }
+	
 }
--- TardeClase09MavenJDBCGenericDAO/src/main/java/ar/org/centro8/curso/java/test/TestGeneric.java
+++ TardeClase09MavenJDBCGenericDAO/src/main/java/ar/org/centro8/curso/java/test/TestGeneric.java
@@ -7,29 +7,33 @@ import ar.org.centro8.curso.java.repositories.GenericR;
 
 public class TestGeneric {
     public static void main(String[] args) {
+		
         System.out.println("--- Prueba de GenericR con clase Alumno ---");
         GenericR<Alumno> ar = new GenericR(Connector.getConnection());
-        ar.save(new Alumno("Nicolas","Leno",22,1));
-        
-        System.out.println("---Prueba de GenericR con clase Curso ---");
-        GenericR<Curso> cr = new GenericR(Connector.getConnection());
-        cr.save(new Curso("Oracle","Hernan","Lunes","Noche"));
-        
-        System.out.println("--- Prueba de GenericR ---");
+        ar.save(new Alumno("Nicolas", "Leno", 22, 1));
+		// Esta primera prueba es para revisar si la query se genera correctamente, sin setId
+		
+		System.out.println("--- Prueba de save() si ingresa la id desde la BD ---");
         Alumno a = new Alumno("Mirta", "Mendoza", 43, 1);
         ar.save(a);
         System.out.println(a);
-        
-        //System.out.println("--- Filtro de Alumnos ---");
+		// Esta prueba revisa si funciona setId correctamente
+		
+		//System.out.println("--- Prueba de getByFiltro() usando Alumnos ---");
         //ar.getByFiltro("1=1", "Alumno").forEach(System.out::println);
+		// Se prueba si getByFiltro() funciona correctamente
+		
+		//System.out.println("--- Prueba de getAll() ---");
+        //ar.getAll("Alumno").forEach(System.out::println);
+		// Se prueba si getAll() funciona correctamente
+		
+        System.out.println("--- Prueba de GenericR con clase Curso ---");
+        GenericR<Curso> cr = new GenericR(Connector.getConnection());
+        cr.save(new Curso("Oracle", "Hernan", "Lunes", "Noche"));
+		cr.save(new Curso("Python", "Matias", "Martes", "Tarde"));
         
-        //System.out.println("--- Filtro de Cursos ---");
+        //System.out.println("--- Prueba de getByFiltro() usando Cursos ---");
         //cr.getByFiltro("1=1", "Curso").forEach(System.out::println);
-        
-        System.out.println("--------------------------");
-        ar.getAll("Alumno").forEach(System.out::println);
-        
-        //gomezjose15386@gmail.com
         
     }
 }
