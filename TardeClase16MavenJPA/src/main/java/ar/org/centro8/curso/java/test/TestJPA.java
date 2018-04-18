18cd6ac7918a4f5846cde4779a77bcfc3a188e96
CodeSeba <seba84x@gmail.com>
2018-04-18 03:25:56 -0300

    Revisado Correcto TardeClase16MavenJPA

--- TardeClase16MavenJPA/src/main/java/ar/org/centro8/curso/java/test/TestJPA.java
+++ TardeClase16MavenJPA/src/main/java/ar/org/centro8/curso/java/test/TestJPA.java
@@ -22,25 +22,29 @@ public class TestJPA {
         System.out.println(alumno);
         
         System.out.println("------ .find() ------");
-		alumno = em.find(Alumno.class, 60);
+		alumno = em.find(Alumno.class, 58);
         System.out.println(alumno);
         
-        if (alumno != null) {
+        System.out.println("------ update ? ------");
+		if (alumno != null) {
             alumno.setNombre("Karina");
             alumno.setApellido("Vazques");
         }
+		System.out.println(em.find(Alumno.class, 58));
         
-        System.out.println("------ .remove() ------");
 		alumno = em.find(Alumno.class, 62);
-        if (alumno != null) em.remove(alumno);
+        if (alumno != null) {
+			System.out.println("------ .remove() ------");
+			em.remove(alumno);
+		}
         
         tx.commit();
         
-        System.out.println("------ NamedQuery('Alumno.findAll',...) ------");
+        System.out.println("------ NamedQuery('Alumno.findAll', Class) ------");
 		em.createNamedQuery("Alumno.findAll", Alumno.class)
                 .getResultList().forEach(System.out::println);
         
-        System.out.println("------ NamedQuery('Alumno.findByApellido',...) ------");
+        System.out.println("------ NamedQuery('Alumno.findByApellido', Class) ------");
         em.createNamedQuery("Alumno.findByApellido", Alumno.class).setParameter("apellido","Picapiedras")
                 .getResultList().forEach(System.out::println);
     }
