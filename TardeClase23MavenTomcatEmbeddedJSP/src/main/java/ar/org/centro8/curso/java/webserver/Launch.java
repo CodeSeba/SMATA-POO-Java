04fed703a8b2eb959c86d4f89100697482c2c79c
CodeSeba <seba84x@gmail.com>
2018-05-05 14:08:22 -0300

    Nombres y TomcatEmbebido 9.0.8

---
+++ TardeClase23MavenTomcatEmbeddedJSP/src/main/java/ar/org/centro8/curso/java/webserver/Launch.java
@@ -0,0 +1,23 @@
+package ar.org.centro8.curso.java.webserver;
+
+import java.io.File;
+import javax.servlet.ServletException;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class Launch {
+
+    public static void main(String[] args) throws LifecycleException,
+            InterruptedException, ServletException {
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+		tomcat.getConnector();
+		
+        String docBase = "src/main/webapp/";
+        tomcat.addWebapp("", new File(docBase).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
