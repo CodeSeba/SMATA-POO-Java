142b2e7f790c35f36b7d0e36fc34f2acce5fdaa8
CodeSeba <seba84x@gmail.com>
2018-05-05 10:04:17 -0300

    TardeClase23TomcatEmbeded estandarizacion del Launch

--- TardeClase23TomcatEmbeded/src/main/java/ar/org/centro8/curso/java/test/TestConnector.java
+++ TardeClase23TomcatEmbeded/src/main/java/ar/org/centro8/curso/java/test/TestConnector.java
@@ -5,22 +5,19 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 public class TestConnector {
-    public static void main(String[] args) throws Exception{
-        Connection conn=Connector.getConnection();
-        String query="insert into alumnos (nombre,apellido,edad,idCurso) values "
-                + "('Juan','Perez',23,1)";
-        Statement st=conn.createStatement();
-        st.execute(query);
-        st.close();
-        //conn.close();
-        
-        Connector.getConnection().createStatement().execute(
-            "insert into alumnos (nombre,apellido,edad,idCurso) values "
-                    + "('Laura','Moretti',22,1)"
-        );
-        
-        
-        
-        
-    }
+
+	public static void main(String[] args) throws Exception {
+		Connection conn = Connector.getConnection();
+		String query = "insert into alumnos (nombre,apellido,edad,idCurso) values "
+				+ "('Juan','Perez',23,1)";
+		Statement st = conn.createStatement();
+		st.execute(query);
+		st.close();
+		//conn.close();
+
+		Connector.getConnection().createStatement().execute(
+				"insert into alumnos (nombre,apellido,edad,idCurso) values "
+				+ "('Laura','Moretti',22,1)"
+		);
+	}
 }
