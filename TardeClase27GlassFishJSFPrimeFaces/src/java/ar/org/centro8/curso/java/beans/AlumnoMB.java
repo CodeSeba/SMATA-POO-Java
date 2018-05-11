214685d69f2e468a468ca1b6119c806671373a61
CodeSeba <seba84x@gmail.com>
2018-05-11 18:22:34 -0300

    Fin TardeClase27...

--- TardeClase27GlassFishJSFPrimeFaces/src/java/ar/org/centro8/curso/java/beans/AlumnoMB.java
+++ TardeClase27GlassFishJSFPrimeFaces/src/java/ar/org/centro8/curso/java/beans/AlumnoMB.java
@@ -3,6 +3,7 @@ package ar.org.centro8.curso.java.beans;
 import ar.org.centro8.curso.java.entities.Alumno;
 import ar.org.centro8.curso.java.repositories.AlumnoR;
 import java.io.Serializable;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -16,28 +17,37 @@ public class AlumnoMB implements Serializable {
 	private EntityManager em;
 	private Alumno alumno;
 	private AlumnoR ar;
+	private String param;
 
 	public AlumnoMB() {
 		em = Persistence.createEntityManagerFactory("JPAPU").createEntityManager();
 		ar = new AlumnoR(em);
 		alumno = new Alumno();
+		param = "";
 	}
 	
 	public void save() {
-		System.out.println("-------------------------------------------------");
-		System.out.println(alumno);
-		System.out.println("-------------------------------------------------");
 		ar.save(alumno);
-		System.out.println("-------------------------------------------------");
-		System.out.println(alumno);
-		System.out.println("-------------------------------------------------");
 		FacesContext
 				.getCurrentInstance()
 				.addMessage(null,
 						new FacesMessage(FacesMessage.FACES_MESSAGES,
 						"Se dio de alta un alumno id " + alumno.getId())
 						);
+		alumno = new Alumno();
 	}
+	
+	public void remove() {
+		ar.remove(ar.getById(alumno.getId()));
+		FacesContext
+				.getCurrentInstance()
+				.addMessage(null,
+						new FacesMessage(FacesMessage.FACES_MESSAGES,
+						"Se dio de baja el alumno id " + alumno.getId())
+						);
+		alumno = new Alumno();
+	}
+	
 
 	public Alumno getAlumno() {
 		return alumno;
@@ -62,4 +72,23 @@ public class AlumnoMB implements Serializable {
 	public void setAr(AlumnoR ar) {
 		this.ar = ar;
 	}
+	
+	public List<Alumno> getAll() {
+		return ar.getAll();
+	}
+	
+	public List<Alumno> getLikeApellido() {
+		if (param.isEmpty()) return ar.getAll();
+		return ar.getLikeApellido(param);
+	}
+
+	public String getParam() {
+		return param;
+	}
+
+	public void setParam(String param) {
+		this.param = param;
+	}
+	
+	
 }
