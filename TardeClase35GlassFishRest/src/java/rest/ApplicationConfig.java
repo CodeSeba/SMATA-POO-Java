39f63580ab2712aeaac02da54e5b3a70606d14b1
CodeSeba <seba84x@gmail.com>
2018-06-04 18:26:30 -0300

    Inicio TardeClase35GlassFishRest...

---
+++ TardeClase35GlassFishRest/src/java/rest/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package rest;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+@javax.ws.rs.ApplicationPath("webResources")
+public class ApplicationConfig extends Application{
+	
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> resources = new HashSet();
+		resources.add(rest.TestRest.class);
+		resources.add(ar.org.centro8.curso.java.services.AlumnoServiceRest.class);
+		return resources;
+	}
+}
